f5deee3896a1f7da5cea24b18ba214864ec0fbea
unknown <tq4@duke.edu>
2014-11-20 20:04:49 -0500

    Cleaned up code for end.ino

--- end/end.ino
+++ end/end.ino
@@ -3,6 +3,7 @@
 #define SCORE 4
 #define IN_HOLEB 5
 #define ON_BOARDB 6
+#define  offset  65
 int scoreA = 0;
 int scoreB = 0;
 
@@ -43,33 +44,7 @@ void loop()
 }
 
 void sendScore(int score) {
-  if (score = 0) {
-    Serial.print('A');
-  } else if (score = 1) {
-    Serial.print('B');
-  } else if (score = 2) {
-    Serial.print('C');
-  } else if (score = 3) {
-    Serial.print('D');
-  } else if (score = 4) {
-    Serial.print('E');
-  } else if (score = 5) {
-    Serial.print('F');
-  } else if (score = 6) {
-    Serial.print('G');
-  } else if (score = 7) {
-    Serial.print('H');
-  } else if (score = 8) {
-    Serial.print('I');
-  } else if (score = 9) {
-    Serial.print('J');
-  } else if (score = 10) {
-    Serial.print('K');
-  } else if (score = 11) {
-    Serial.print('L');
-  } else if (score = 12) {
-    Serial.print('M');
-  }
+  score.char()
 }
 
 void reset() {
